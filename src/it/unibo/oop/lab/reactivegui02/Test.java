c1b5a7f7c1eb230c921b7a139d9e65dbfc6414ab
HDavi <davide.merli5@studio.unibo.it>
2020-12-01 17:10:54 +0100

    Updated class reactivegui02.Test

--- src/it/unibo/oop/lab/reactivegui02/Test.java
+++ src/it/unibo/oop/lab/reactivegui02/Test.java
@@ -7,7 +7,7 @@ public final class Test {
 
     private Test() {
     }
-
+    
     /*
      * Realizzare una classe ConcurrentGUI con costruttore privo di argomenti,
      * tale che quando istanziata crei un JFrame con l'aspetto mostrato nella
@@ -39,6 +39,6 @@ public final class Test {
      * 
      */
     public static void main(final String... args) {
-        // new ConcurrentGUI();
+        new ConcurrentGUI();
     }
 }
